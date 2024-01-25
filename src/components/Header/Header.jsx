e215def494bdf367ae03e0f31010372f474ffe98
ge1118 <greean246@gmail.com>
2024-01-24 21:09:51 -0500

    error solved: header section - responsive nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
 import Bars from '../../assets/bars.png'
+import Close from '../../assets/close.png'
 import { Link } from 'react-scroll'
 
 const Header = () => {
 
-  const mobile = window.innerWidth <= 768 ? true : false
-  const [menuOpened, setMenuOpened] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
+  const [isMenuShown, setIsMenuShown] = useState(false)
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+
+    window.addEventListener('resize', checkIfMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkIfMobile);
+    }
+  }, []);
+
+  const closeMenu = () => setIsMenuShown(false);
 
   return (
     <div>
       <div className="header">
-        <img src={Logo} alt="" className='logo' />
-        {(menuOpened === false && mobile === true) ?
-          (
-            <div onClick={() => setMenuOpened(true)}>
-              <img src={Bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
-            </div>
-          ) :
+        <img src={Logo} alt="logo" className='logo' />
+        {(isMobile) &&
+          <div onClick={() => setIsMenuShown(!isMenuShown)}>
+            <img src={isMenuShown ? Close : Bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
+          </div>
+        }
+
+        {(isMenuShown || !isMobile) ?
           (
             <ul className='header-menu'>
               <li><Link
@@ -26,33 +42,31 @@ const Header = () => {
                 spy={true}
                 smooth={true}
                 activeClass='active'
-                onClick={() => { setMenuOpened(false) }}>Home</Link></li>
+                onClick={closeMenu}>Home</Link></li>
               <li><Link
                 to='programs'
                 spy={true}
                 smooth={true}
-                onClick={() => { setMenuOpened(false) }}>Programs</Link></li>
+                onClick={closeMenu}>Programs</Link></li>
               <li><Link
                 to='reasons'
                 spy={true}
                 smooth={true}
-                onClick={() => { setMenuOpened(false) }}>Why Us</Link></li>
+                onClick={closeMenu}>Why Us</Link></li>
               <li><Link
                 to='plans-container'
                 spy={true}
                 smooth={true}
-                onClick={() => { setMenuOpened(false) }}>Plans</Link></li>
+                onClick={closeMenu}>Plans</Link></li>
               <li ><Link
                 to='testimonials'
                 spy={true}
                 smooth={true}
-                onClick={() => { setMenuOpened(false) }}>Testimonials</Link></li>
+                onClick={closeMenu}>Testimonials</Link></li>
             </ul>
-          )}
-
-
+          ) : ''}
       </div>
-    </div>
+    </div >
   )
 }
 
